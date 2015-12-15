b5c032dae8d244931631dc144066b939e11adcd5
xuelinf <xuelinf@qq.com>
2015-12-15 15:43:36 +0800

    80% completed

---
+++ blockDropping/blockDropping/Swiftris.swift
@@ -0,0 +1,41 @@
+//
+//  Swiftris.swift
+//  blockDropping
+//
+//  Created by xuelinf on 15/12/15.
+//  Copyright © 2015年 xuelinf. All rights reserved.
+//
+
+let NumColumns = 10
+let NumRows = 20
+
+let StartingColumn = 4
+let StartingRow = 0
+
+let PreviewColumn = 12
+let PreviewRow = 1
+
+class Swiftris {
+    var blockArray:Array2D<Block>
+    var nextShape:Shape?
+    var fallingShape:Shape?
+    
+    init() {
+        fallingShape = nil
+        nextShape = nil
+        blockArray = Array2D<Block>(columns: NumColumns, rows: NumRows)
+    }
+    
+    func beginGame() {
+        if (nextShape == nil) {
+            nextShape = Shape.random(PreviewColumn, startingRow: PreviewRow)
+        }
+    }
+    
+    func newShape() -> (fallingShape:Shape?, nextShape:Shape?) {
+        fallingShape = nextShape
+        nextShape = Shape.random(PreviewColumn, startingRow: PreviewRow)
+        fallingShape?.moveTo(StartingColumn, row: StartingRow)
+        return (fallingShape, nextShape)
+    }
+}
